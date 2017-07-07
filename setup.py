d1ce924372b815e7f215a946dd1361a3b55ab60b
Thien Bui <thienbui797@gmail.com>
2017-07-08 00:29:35 +0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='FaceExtractor',
+      version='0.1',
+      description='Extract faces from images with OpenCV',
+      url='https://github.com/ducthienbui97/FaceExtractor',
+      author='Thien Bui',
+      author_email='thienbui797@gmail.com',
+      license='BSD-3-Clause',
+      packages=['face_extractor'],
+      install_requires=['opencv-python'],
+      package_data={'face_extractor':['data/*.xml']},)
